06802fc4eb418787abd39899d6667878f5a1550f
Christina <github email address>
2018-08-07 18:32:30 -0600

    added uuidV4 to yarn.lock and code in the DogFood.js file to specifically identify elements.

--- src/components/DogFood.js
+++ src/components/DogFood.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import DogForm from './DogForm';
 import Dogs from './Dogs';
+import uuidV4 from 'uuid/v4';
 
 export default class DogFood extends Component {
   constructor(props) {
@@ -11,11 +12,21 @@ export default class DogFood extends Component {
     }
   }
 
-addDog = (dog) => {
-  this.setState({
-    dogs: [this.state.dogs, dog],
-  })
-}
+  componentWillMount = () => {
+    this.setState({
+      dogs: [
+        ...this.state.dogs,
+        {id: uuidV4(), name: "Fido", weight: "3", bagWeight: "32"},
+        {id: uuidV4(), name: "Max", weight: "4", bagWeight: "52"}
+      ]
+    });
+  }
+
+  addDog = (dog) => {
+    this.setState({
+      dogs: [...this.state.dogs, {id: uuidV4(), ...dog}],
+    })
+  }
 
   render() {
     return (
