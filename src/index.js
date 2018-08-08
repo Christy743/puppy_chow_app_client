bd683aace0c35b6a0d6bcd9a3e49d2f4ae1d7387
Christina <github email address>
2018-08-08 11:04:58 -0600

    added redux to client app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from 'redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
+
+const composeEnhancers = composeWithDevTools({});
+const store = createStore(
+  rootReducer,
+    composeEnhancers()
+);
 
 ReactDOM.render(
   <BrowserRouter>
